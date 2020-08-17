c07dd47ada09ce5ca12db6afa8b8c220f0a46053
azmi-wijaya <55981660+azmi-wijaya@users.noreply.github.com>
2020-08-17 21:25:14 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const axios = require("axios")
+const urls = ["https://yui-music.glitch.me"]
+setInterval(function() {
+            urls.forEach(url => {
+            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+        })
+    }, 60 * 1000);
